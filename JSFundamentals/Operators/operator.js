5c0278050af6c94589a16666025d3f3787516060
Ratheshprabakar <ratheshprabakar@gmail.com>
2021-06-21 14:25:01 +0530

    Added Operators demo

---
+++ JSFundamentals/Operators/operator.js
@@ -0,0 +1,22 @@
+//Equal operators (==)
+ var a = 100;
+ var str = "10";
+ if (a == str) {
+ console.log("Equals to");
+ } else {
+ console.log("not equal to");
+ }
+
+ //strict not equal to
+ var a = "100";
+ var str = "100"; //not equal datatype is same but not values
+ if (a === str) {
+ console.log("Equals to");
+ } else {
+ console.log("not equal to");
+ }
+
+ // //ternary operator
+ var age = prompt("enter ur age");
+ var status = age >= 18 ? "Adult" : "Minor";
+ console.log(status);
