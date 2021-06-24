14460a9df22ef3dae79b77a8d6016f17764d9454
Ratheshprabakar <ratheshprabakar@gmail.com>
2021-06-24 10:25:50 +0530

    Added DOM Methods, Events,Destructuring and ES6

--- Callbacks/callback.js
+++ Callbacks/callback.js
@@ -1,1 +1,11 @@
-
+// synchronous callback,(executed immediately)
+function greeting(name) {
+    alert('Hello ' + name);
+   }
+   
+   function userInput(status) {
+    var name = prompt('Please enter your name');
+    status(name);
+   }
+   
+   userInput(greeting);
